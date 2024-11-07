9b40fc40b450e44cdd30f484d0c386d0d529c612
Hrishikesh M Rao <96303885+HrishikeshMRao@users.noreply.github.com>
2024-11-07 14:05:36 +0530

    Update freertos.c

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -12,9 +12,9 @@ static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
 
 void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
 {
-  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
-  *ppxIdleTaskStackBuffer = &xIdleStack[0];
-  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer; //Assigning xIdleTaskTCBBuffer: The function sets *ppxIdleTaskTCBBuffer to the address of xIdleTaskTCBBuffer. This pointer provides FreeRTOS with the memory for managing the idle task's state.
+  *ppxIdleTaskStackBuffer = &xIdleStack[0]; //Assigning xIdleStack: The function sets *ppxIdleTaskStackBuffer to the base address of xIdleStack. This tells FreeRTOS where to place the idle task's stack.
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE; //Setting the Stack Size: The function assigns the value of configMINIMAL_STACK_SIZE to *pulIdleTaskStackSize. FreeRTOS uses this value to know how much stack memory it can use for the idle task.
 
 }
 
